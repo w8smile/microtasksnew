35a74a3c086d363cb1996040b59aa1058e5b57fc
Serg Nakovnick <w8smile@gmail.com>
2024-01-09 16:03:38 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,38 @@
 import './App.css'
 import React, {useState} from "react";
-
-type FilterType = "all" | "dollar" | "ruble";
+import {FullInput} from "./components/FullInput";
 
 function App() {
-    const [money, setMoney] = useState([
-        {banknote: "dollar", nominal: 100, number: "a123456789"},
-        {banknote: "dollar", nominal: 50, number: "b123456789"},
-        {banknote: "ruble", nominal: 100, number: "c123456789"},
-        {banknote: "dollar", nominal: 100, number: "d123456789"},
-        {banknote: "dollar", nominal: 50, number: "e123456789"},
-        {banknote: "ruble", nominal: 100, number: "f123456789"},
-        {banknote: "dollar", nominal: 50, number: "j123456789"},
-        {banknote: "ruble", nominal: 50, number: "h123456789"}
-    ])
-    let [filter, setFilter] = useState<FilterType>('all')
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage,...message])
 
-    let currentMoney = money;
-    if (filter==='dollar') {
-        currentMoney=money.filter((f)=>f.banknote==='dollar')
-    }
-    if (filter==='ruble') {
-        currentMoney=money.filter((f)=>f.banknote==='ruble')
     }
 
-    const OnClickHandler = (filterValue: FilterType) => {
-        setFilter(filterValue)
-    }
 
 
     return (
-        <div className='App'>
-                {currentMoney.map((el, index)=> {
-                    return(
-                        <li key={index}>
-                            <span>{el.banknote}</span>
-                            <span> {el.nominal}</span>
-                            <span> {el.number}</span>
-                        </li>
-                    )
-                })}
-            <button onClick={()=> OnClickHandler ('all')}>ALL</button>
-            <button onClick={()=> OnClickHandler ('dollar')}>DOLLAR</button>
-            <button onClick={()=> OnClickHandler ('ruble')}>RUBLE</button>
-
-
+        <div className="App">
+            <FullInput
+                addMessage={addMessage}/>
+            {/*<div>*/}
+            {/*    <input />*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
         </div>
-
     );
 }
 
---
+++ src/components/FullInput.tsx
@@ -0,0 +1,25 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type FullInputType = {
+    addMessage: (title: string)=>void
+}
+export const FullInput = (props: FullInputType) => {
+
+    let [title,setTitle] = useState('')
+    const OnChangeHandler = (event: ChangeEvent<HTMLInputElement>)=> {
+        setTitle(event.currentTarget.value)
+    }
+
+    const OnClickHandler = () => {
+        props.addMessage(title)
+        setTitle('')
+    }
+    return (
+        <div>
+            <input value={title} onChange={OnChangeHandler}/>
+            <button onClick={OnClickHandler}>+</button>
+        </div>
+    );
+};
+
+export default FullInput;
